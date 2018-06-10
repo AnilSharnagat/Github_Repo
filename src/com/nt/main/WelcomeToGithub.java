6aaa63c05584861db7667195a748d27974651aba
AnilSharnagat <anil5sharnagat@gmail.com>
2018-06-10 19:25:45 +0530

    First commit

---
+++ src/com/nt/main/WelcomeToGithub.java
@@ -0,0 +1,14 @@
+package com.nt.main;
+
+public class WelcomeToGithub {
+
+	public static void main(String[] args) {
+		System.out.println("WEL-COME");                   
+		System.out.println("-------------------------------------------");                   
+		System.out.println("Github   standAlon Project. ");    
+		System.out.println("-------------------------------------------");                   
+		System.out.println("Thank You. ");    
+	
+	}
+
+}
